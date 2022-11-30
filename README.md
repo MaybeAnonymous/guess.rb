268ca9a6a5aa948b0e144b97b4f1f121027a88df
MaybeAnonymous <89218161+MaybeAnonymous@users.noreply.github.com>
2022-11-30 14:14:22 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# guess.rb
+
+A number guessing game written in Ruby.
+
+## Dependencies
+
+### [ruby](https://www.ruby-lang.org/en/)
+
+* Installation depends on your system, please check the website.
+
+### [rainbow](https://rubygems.org/gems/rainbow)
+	
+* Gem used for coloring terminal text.
+* `gem install rainbow`
+
+## How to run
+
+* `ruby guess.rb`
+
---
+++ guess.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rainbow'
+
+class Config
+  attr_reader :min, :max, :tries
+
+  def initialize
+    @min = 0
+    @max = 100
+    @tries = 8
+  end
+end
+
+config = Config.new
+number = rand(config.min..config.max)
+
+# Uncomment this line to get the answer
+# puts number
+
+puts "Welcome! Guess a number between #{config.min} and #{config.max}. You have #{config.tries} tries."
+
+config.tries.times do
+  begin
+    print Rainbow('Your guess: ').yellow
+    guess = gets.chomp.to_i
+  rescue SystemExit, Interrupt
+    puts "\nGoodbye!"
+    exit
+  end
+  if guess == number
+    puts 'You win!'
+    exit
+  elsif guess > number
+    puts 'Too big, try a lower guess.'
+  elsif guess < number
+    puts 'Too small, try a higher guess.'
+  end
+end
+puts "You lose! It was #{number}"
